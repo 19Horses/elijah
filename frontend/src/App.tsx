1f2822f4d688bd3fe68f0f6d3fdc01a8c6a28583
Cammy Docherty <cammy.docherty@klaviyo.com>
2026-05-26 09:23:33 +0100

    React router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
+import Shop from './pages/Shop';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <HashRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </main>
+      </HashRouter>
     </QueryClientProvider>
   );
 };
